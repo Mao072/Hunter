5f2e7f17e0833589f06a2c3336d2cf236142d1c8
ryan <ryanwu0517@gmail.com>
2024-06-20 03:11:35 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = defineConfig({
     //可使用 **process.env.NODE_ENV** 來判斷是否為發布而修改發佈位置
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/data_add_serial_number/'
-      : '/'
+    publicPath:'/Hunter/'
   })
